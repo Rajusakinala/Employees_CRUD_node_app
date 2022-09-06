e38f6b06a66eb2a69e1cabb4e9744b92462fad6c
Rajusakinala <sakinala.raju1@gmail.com>
2022-09-06 16:12:16 +0530

    adding user model

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require('express');
 require('./db/mongoose');
 const app = express();
 const employeeRouter = require('./routers/employee')
+const userRouter = require('./routers/user');
 const taskRouter = require('./routers/task')
 
 app.use(express.json());
 app.use(employeeRouter);
+app.use(userRouter);
 app.use(taskRouter);
 
 const port = process.env.PORT || 3006;
